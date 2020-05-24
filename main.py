680691ea907306f6080a51a17bdedf26126e070e
JetBox <zeldagameremail@gmail.com>
2020-05-24 14:14:21 -0700

    fixed formatting

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from numpy.linalg import inv
 from numpy import dot, array, diag
 from scipy.stats import t
 
+
 class LinearRegression:
     def __init__(self):
         self.coefficients = array([])
@@ -11,17 +12,17 @@ class LinearRegression:
         self.features = 0
         self.training_n = 0
 
-    def fit(self, design:array, response:array):
+    def fit(self, design: array, response: array):
         """Fit the linear regression model to the training data."""
         """The least-squares normal equation is used to estimate the coefficients."""
-        
-        self.coefficients = dot(inv(dot(design.T, design)), dot(design.T, response)) 
+
+        self.coefficients = dot(inv(dot(design.T, design)), dot(design.T, response))
         self.training_design = design
         self.training_response = response
         self.features = len(design.T)
         self.training_n = len(design)
 
-    def predict(self, predictors:array, design=False):
+    def predict(self, predictors: array, design=False):
         """Use the coefficients to predict the response(s) for given values for the predictors"""
         """If predictors is in design-matrix format then design should be True"""
         if self.coefficients.size == 0:
@@ -31,11 +32,12 @@ class LinearRegression:
         else:
             return dot(add_1s_column(predictors), self.coefficients)
 
-    def R2(self, test_predictors:array, test_responses:array, design=False):
+    def R2(self, test_predictors: array, test_responses: array, design=False):
         """Compute the fraction of variance in the response explained by the predictors"""
-        return (1 - self.RSS(test_predictors, test_responses, design)/sum([(y-test_responses.mean())**2 for y in test_responses]))[0]
+        return (1 - self.RSS(test_predictors, test_responses, design) / sum(
+            [(y - test_responses.mean()) ** 2 for y in test_responses]))[0]
 
-    def RSS(self, test_predictors:array, test_responses:array, design=False):
+    def RSS(self, test_predictors: array, test_responses: array, design=False):
         """Compute the RSS for a given set of data. Set design=True if test_predictors is given as a design matrix"""
         if design:
             errors = test_responses - dot(test_predictors, self.coefficients)
@@ -46,18 +48,19 @@ class LinearRegression:
 
     def RSE2(self):
         """Compute the squared RSE (Residual standard error), an estimate for the variance of e in Y = f(X) + e"""
-        return self.RSS(self.training_design, self.training_response, design=True)/(self.training_n - self.features - 1)
+        return self.RSS(self.training_design, self.training_response, design=True) / (
+                    self.training_n - self.features - 1)
 
     def coefficient_CI(self, confidence_level):
         """Compute the confidence interval for the regression coefficients at the confidence_level level of confidence"""
         """confidence_level should be in (0.00, 1.00)"""
         """RSE2() * inv(dot(...)) returns the covariance matrix for the """
         """The confidence interval is derived from (Bj - Bjhat)/SE(Bjhat) ~ t_{n-p-1}"""
-        
+
         if not 0 < confidence_level < 1:
             raise Exception("Invalid level of confidence; confidence_level must be between 0 and 1")
-        t_value = t.ppf(confidence_level/2 + 0.5, self.training_n - self.features - 1)
-        coeff_se = (diag(self.RSE2() * inv(dot(self.training_design.T, self.training_design))))**.5
+        t_value = t.ppf(confidence_level / 2 + 0.5, self.training_n - self.features - 1)
+        coeff_se = (diag(self.RSE2() * inv(dot(self.training_design.T, self.training_design)))) ** .5
         result = []
 
         for (i, se) in enumerate(coeff_se):
@@ -67,12 +70,18 @@ class LinearRegression:
 
         return result
 
+
 if __name__ == "__main__":
     data = read_data("data.csv")
     model = LinearRegression()
     model.fit(data[0], data[1])
+    print("Predict X1 = 10, X2 = 3: ",end=" ")
     print(model.predict(array([[10, 3]])))
+    print("Minimized RSS: ", end= " ")
     print(model.RSS(data[0], data[1], True))
+    print("Squared RSE: ", end=" ")
     print(model.RSE2())
+    print("95% Confidence Interval: ", end=" ")
     print(model.coefficient_CI(0.95))
-    print(model.R2(data[0],data[1],True))
+    print("R^2: ", end=" ")
+    print(model.R2(data[0], data[1], True))
--- read_data.py
+++ read_data.py
@@ -1,11 +1,13 @@
 from csv import reader
 from numpy import array, append, asfarray
 
-def add_1s_column(matrix:array) -> array:
-    """Add a ones column to the left-most column of the array"""
-    return append(array([[1]*len(matrix)]).T, matrix, axis=1)
 
-def read_data(data_path:str, delimiter=",", use_headers=False) -> array:
+def add_1s_column(matrix: array) -> array:
+    """Add a ones column to the left-most column of the array"""
+    return append(array([[1] * len(matrix)]).T, matrix, axis=1)
+
+
+def read_data(data_path: str, delimiter=",", use_headers=False) -> array:
     """Read data from the .csv file. Return the design matrix and the response values"""
     """If there is an error in the .csv file (for example, a non-numerical entry)"""
     """use_headers refers to whether or not the .csv file uses headers (for example, x1,x2,x3,y on the first row)"""
@@ -13,15 +15,12 @@ def read_data(data_path:str, delimiter=",", use_headers=False) -> array:
 
     with open(data_path, "r") as datafile:
         data_reader = reader(datafile, delimiter=delimiter)
-        
-        response = array([])
-        
+
         rows = array([row for row in data_reader])
-        rows = rows[1:] if use_headers else rows # If headers are used, omit them, otherwise do nothing
-        features = len(rows[0])-1 # Every row has has values for X1, X2, ... , Xp and y, this is features+1 columns
-                                  # index of the response vector in rows = features
+        rows = rows[1:] if use_headers else rows  # If headers are used, omit them, otherwise do nothing
+        features = len(rows[0]) - 1  # Every row has has values for X1, X2, ... , Xp and y, this is features+1 columns
+                                     # index of the response vector in rows = features
 
-        design_matrix = add_1s_column(rows[:,:features])
-        
-        return (asfarray(design_matrix, float), asfarray(rows[:,features].reshape((-1,1)), float))
+        design_matrix = add_1s_column(rows[:, :features])
 
+        return asfarray(design_matrix, float), asfarray(rows[:, features].reshape((-1, 1)), float)
