1e9f88664d1f5fc6a0483a49ee2c64ef52844c4b
JetBox <zeldagameremail@gmail.com>
2020-05-26 12:44:14 -0700

    fixed leftover references

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from read_data import read_data
 import linear_regression as linear_regression
 from numpy import array
-import twitter_scraper
 
 
-def data_test(training_data=None, read_from_file=True):
+def data_test():
     model = linear_regression.LinearRegression()
-    if read_from_file or training_data is None:
-        training_data = read_data("data.csv")
+    training_data = read_data("data.csv")
 
     model.fit(training_data[0], training_data[1])
     print(model.coefficients)
-    if read_from_file or training_data is None:
-        print("Predict X1 = 10, X2 = 3: ", end=" ")
-        print(model.predict(array([[10, 3]])))
-    else:
-        print("Predict X1 = 0, X2 = 0, X3 = 0, X4 = 1: ", end=" ")
-        print(model.predict(array([[0, 0, 0, 1]])))
+    print("Predict X1 = 10, X2 = 3: ", end=" ")
+    print(model.predict(array([[10, 3]])))
     print("Minimized RSS: ", end=" ")
     print(model.calculate_rss(training_data[0], training_data[1], True))
     print("Squared RSE: ", end=" ")
@@ -37,10 +31,6 @@ def data_test(training_data=None, read_from_file=True):
 
 
 if __name__ == "__main__":
-    my_bot = twitter_scraper.TwitterScraper()
-    tweets = my_bot.scrape_user('BarackObama', 500)
-    tweets = twitter_scraper.remove_retweets(tweets)
-    data = twitter_scraper.format_tweets_as_training_data(tweets)
 
-    data_test(data, False)
+    data_test()
     print("X0 = intercept, X1 = # of retweets, X2 = has_media, X3 = is_reply, X4 = is_quote_tweet")
